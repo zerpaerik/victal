d729e476daf97c035e91262e62989602f507210a
zerpaerik <zerpaerik@gmail.com>
2022-01-15 17:56:03 -0400

    aaa

--- resources/views/consultas/historia-pdf.blade.php
+++ resources/views/consultas/historia-pdf.blade.php
@@ -71,7 +71,7 @@ table {
 
 		<div>
 			<div align="left" style="width: 50%;">
-				<img src="/var/www/html/medical-prevent/public/img/medical-prevent.jpeg"  style="width: 20%;"/>
+				<img src="/var/www/html/victal/public/victal.png"  style="width: 20%;"/>
 			</div>
 
 		</div>
