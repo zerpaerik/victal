eaab2297f572955b3139c23e726e2d8138ccb5a3
zerpaerik <zerpaerik@gmail.com>
2022-03-08 09:31:12 -0400

    aa

--- resources/views/resultados/pdf.blade.php
+++ resources/views/resultados/pdf.blade.php
@@ -1,23 +1,18 @@
 <html>
     <head>
         <style>
-            /** 
-                Establezca los márgenes de la página en 0, por lo que el pie de página y el encabezado
-                puede ser de altura y anchura completas.
-             **/
+            /** Define the margins of your page **/
             @page {
                 margin: 0cm 0cm;
             }
 
-            /** Defina ahora los márgenes reales de cada página en el PDF **/
             body {
                 margin-top: 3cm;
                 margin-left: 2cm;
                 margin-right: 2cm;
-                margin-bottom: 2cm;
+                margin-bottom: -10cm;
             }
 
-            /** Definir las reglas del encabezado **/
             header {
                 position: fixed;
                 top: 0cm;
@@ -32,15 +27,16 @@
                 bottom: 0cm; 
                 left: 0cm; 
                 right: 0cm;
-                height: 4cm;
+                height: 2cm;
             }
 
-			.row{
-		width: 1024px;
-		margin: 0 auto;
-	}
+            table{
+                width: 100%;
+                text-align: center;
+                /*margin: 10px 0;*/
+            }
 
-	.col-12{
+            .col-12{
 		width: 100%;
 	}
 
@@ -64,6 +60,18 @@
 		font-size: 18px;
 	}
 
+    .col-8{
+		width: 75%;
+		float: left;
+		padding: 8px 5px;
+		font-size: 18px;
+	}
+
+    .col-12{
+		width: 100%;
+		font-size: 16px;
+	}
+
 	.text-center{
 		text-align: center;
 	}
@@ -84,7 +92,7 @@
 	table{
 		width: 100%;
 		text-align: center;
-		margin: 10px 0;
+		/*margin: 10px 0;*/
 	}
 	
 	tr th{
@@ -97,112 +105,60 @@
 		font-size: 14px;
 		padding: 8px 5px;
 	}
-
-	/** 
-            * Defina el ancho, alto, márgenes y posición de la marca de agua.
-            **/
-            #watermark {
-                position: fixed;
-                bottom:   0px;
-                left:     0px;
-                /** El ancho y la altura pueden cambiar
-                    según las dimensiones de su membrete
-                **/
-                width:    21.8cm;
-                height:   28cm;
-
-                /** Tu marca de agua debe estar detrás de cada contenido **/
-                z-index:  -1000;
-            }
-
-
-
+	
         </style>
     </head>
     <body>
-        <!-- Defina bloques de encabezado y pie de página antes de su contenido -->
+        <!-- Define header and footer blocks before your content -->
         <header>
-            <img src="header.PNG" width="100%" height="100%"/>
+        <img src="header.PNG" width="100%" height="100%"/>
         </header>
 
         <footer>
-            <img src="footer2.PNG" width="100%" height="100%"/>
+        <img src="footer2.PNG" width="100%" height="100%"/>
         </footer>
 
-		
-
-        <!-- Envuelva el contenido de su PDF dentro de una etiqueta principal -->
-        <div>
-            <div class="col-6" >
-            <strong style="margin-left: -5px;">  Pac: </strong>  {{ $res_i->apellidos.' '.$res_i->nombres}}
-            </div>
-            <div class="col-3">
-            <strong>{{ $res_i->tipo_doc}}:</strong> {{ $res_i->dni}} 
-            </div> 
-            <div class="col-3">
-            <strong> Edad: </strong> {{ $edad}}
-            </div> 
-        </div>
-        <div style="margin-top: -40px;">
-            <div class="col-12" >
-            <strong>  Examen: </strong>  {{ $res_i->detalle}}
-            </div>
-        </div>
-
-		
-
-    <!-- No podrá ver este texto. -->
+        <!-- Wrap the content of your PDF inside a main tag -->
+        <main>
+        <strong style="margin-top: -10px;">Paciente:</strong> {{ $res_i->apellidos.' '.$res_i->nombres}} - 
+        <strong>{{ $res_i->tipo_doc}}:</strong> {{ $res_i->dni}} - 
+        <strong>Edad:</strong> {{ $edad}}<br>
+        <strong>Examen:</strong> {{ $res_i->detalle}}
 
 
+        <table style="">
+                <thead>
+                
+                    <tr>
+                    <th style="width:25%;"></th>
+                    <th style="width:25%;" >Nombre</th>
+                    <th style="width:15%;" >Resultado</th>
+                    <th style="width:15%;" >UM</th>
+                    <th style="width:35%;text-overflow:ellipsis;" scope="col">Rango de Referencia</th>
+
+                
+                
+                
+                </thead>
+        <tbody>
+            @foreach($res as $r)
+            <tr>
+            <td style="padding: 0;background: #ECE6EF;font-size: 10px;"><strong>{{$r->subtitulo}}<strong></td>
+            <br>
+            <tr>
+            <td style="padding: 0;font-size: 12px;"><strong><strong></td>
+            <td style="padding: 0;font-size: 12px;">{{$r->nom_val}}</td>
+            <td style="padding: 0;font-size: 12px;">{{$r->valor}}</td>
+            <td style="padding: 0;font-size: 12px;">{{$r->medida}}</td>
+            <td style="padding: 0;font-size: 10px;">{{$r->referencia}}</td>
+            @endforeach
+
+        </tbody>
 
 
-
-       <!-- <div>
-        <p style="margin-top: 25px;"><strong>Edad:</strong> {{ $edad}}</p>
-        <p style="margin-top: -13px;"><strong>Fecha: </strong> {{ date('d-m-Y', strtotime($res_i->created_at))}}</p>
-        @if($res_i->tipo_origen == 10)
-        <p style="margin-top: -13px;"><strong>Empresa: </strong> {{ $res_i->name}}</p>
-        @endif
-        </div>-->
-
-
-  <br><br><br>
-
-
-
-<table style="margin-top: -5px;border: none;border-collapse:collapse;">
-  <thead>
-  
-    <tr>
-    <th style="width:25%;"></th>
-    <th style="width:25%;" >Nombre</th>
-    <th style="width:15%;" >Resultado</th>
-	<th style="width:15%;" >UM</th>
-    <th style="width:35%;text-overflow:ellipsis;" scope="col">Rango de Referencia</th>
-
-  
-  
- 
-  </thead>
-  <tbody>
-    @foreach($res as $r)
-    <tr>
-    <td style="padding: 0;background: #ECE6EF;"><strong>{{$r->subtitulo}}<strong></td>
-    <br>
-    <tr>
-    <td style="padding: 0;font-size: 12px;"><strong><strong></td>
-	<td style="padding: 0;font-size: 12px;">{{$r->nom_val}}</td>
-    <td style="padding: 0;font-size: 12px;">{{$r->valor}}</td>
-	<td style="padding: 0;font-size: 12px;">{{$r->medida}}</td>
-    <td style="padding: 0;font-size: 10px;">{{$r->referencia}}</td>
-    @endforeach
-
- </tbody>
-
-
-</table>
-
-
-
-</body>
+    </table>
+           
+            
+        </main>
+    </body>
 </html>
